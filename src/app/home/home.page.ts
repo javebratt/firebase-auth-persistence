3ea2203247431ccc1ff897017b38adfefbb47b13
Jorge Vergara <jorge@jorgevergara.co>
2022-04-02 18:58:45 -0500

    adds signup function

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,8 @@ import {
   browserSessionPersistence,
   inMemoryPersistence,
   UserCredential,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
 } from '@angular/fire/auth';
 
 @Component({
@@ -17,9 +19,18 @@ import {
 export class HomePage {
   constructor(private readonly auth: Auth) {}
 
-  login(email: string, password: string): Promise<UserCredential> {
-    return setPersistence(this.auth, browserLocalPersistence).then(() =>
-      signInWithEmailAndPassword(this.auth, email, password)
+  async login(email: string, password: string): Promise<UserCredential> {
+    await setPersistence(this.auth, browserLocalPersistence);
+    return await signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  async signup(email: string, password: string): Promise<UserCredential> {
+    const userCredential = await createUserWithEmailAndPassword(
+      this.auth,
+      email,
+      password
     );
+    await sendEmailVerification(userCredential.user);
+    return userCredential;
   }
 }
